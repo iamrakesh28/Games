e77fef1278cd6f61c562d7953f8f7e426c61063f
root <root@localhost.localdomain>
2019-03-09 13:16:09 +0530

    Block Games

---
+++ block.py
@@ -0,0 +1,113 @@
+import queue
+import os
+import visual
+'''
+	col -> column size
+	returns cell coordinates
+'''
+row = 4
+col = 4
+memo = {}
+st = []
+def cell(num):
+	return (num//col,num%col)
+
+def cellInv(i,j):
+	return i*col + j
+'''
+	|0|1|2|
+	|3|4|5|
+	|6|7|8|
+	(target block,obstacles...)
+'''
+def matrix(start):
+	return (start,2,3)
+
+def path(par,cur):
+	p = []
+	while cur != 0:
+		p.append(cur)
+		cur = par[cur]
+	p.append(0)
+	p.reverse()
+	return p
+def solver(start,end):
+	count = 0
+	memo[start] = count
+	st.append(start)
+	count += 1
+	q = queue.Queue()
+	q.put(0)
+	parent = []
+	parent.append(0)
+	if(start[0] == end):
+		return True
+	while(q.empty() == False):
+		cnt = q.get()
+		p = st[cnt]
+		for x in range(0,len(p)):
+			r,c = cell(p[x])
+			if r >= 1:
+				val = cellInv(r-1,c)
+				state = p[0:x] + (val,) + p[x+1:len(p)]
+				if state not in memo and val not in p:
+					memo[state] = count
+					st.append(state)
+					parent.append(cnt)
+					q.put(count)
+					if(state[0] == end):
+						return path(parent,count)
+					count += 1
+			if r < row-1:
+				val = cellInv(r+1,c)
+				state = p[0:x] + (val,) + p[x+1:len(p)]
+				if state not in memo and val not in p:
+					memo[state] = count
+					st.append(state)
+					parent.append(cnt)
+					q.put(count)
+					if(state[0] == end):
+						return path(parent,count)
+					count += 1
+			if c >= 1:
+				val = cellInv(r,c-1)
+				state = p[0:x] + (val,) + p[x+1:len(p)]
+				if state not in memo and val not in p:
+					memo[state] = count
+					st.append(state)
+					parent.append(cnt)
+					q.put(count)
+					if(state[0] == end):
+						return path(parent,count)
+					count += 1
+			if c < col-1:
+				val = cellInv(r,c+1)
+				state = p[0:x] + (val,) + p[x+1:len(p)]
+				if state not in memo and val not in p:
+					memo[state] = count
+					st.append(state)
+					parent.append(cnt)
+					q.put(count)	
+					if(state[0] == end):
+						return path(parent,count)
+					count += 1
+	return []
+
+end = 15
+os.system('clear')
+p = solver((0,10,11,13,14),end)
+if p == []:
+	print('No solution')
+else:
+	for x in p:
+		q = st[x]
+		q = q + (end,)
+		mat = [col*['.'] for r in range(row)]
+		r,c = cell(q[len(q)-1])
+		mat[r][c] = 'E'
+		r,c = cell(q[0])
+		mat[r][c] = 'S'
+		for i in range(1,len(q)-1):
+			r,c = cell(q[i])
+			mat[r][c] = '#'
+		visual.display(row,col,mat)
---
+++ blockRect.py
@@ -0,0 +1,93 @@
+import queue
+import os
+import visual
+
+row = 5
+col = 5
+memo = {}
+st = []
+dy = [-1,0,0,+1]
+dx = [0,-1,1,0]
+
+def cell(num):
+	return (num//col,num%col)
+
+def cellInv(i,j):
+	return i*col + j
+
+def isPossible(i,j):
+	return (i >= 0 and j >= 0 and i < row and j < col)
+
+def isValid(p,val,i):
+	for x,y in p:
+		if (x,y) != i :
+			if val[0] in (x,y) or val[1] in (x,y):
+				return False
+	return True
+
+def path(par,cur):
+	p = []
+	while cur != 0:
+		p.append(cur)
+		cur = par[cur]
+	p.append(0)
+	p.reverse()
+	return p
+
+def solver(start,end):
+	count = 0
+	memo[start] = count
+	st.append(start)
+	count += 1
+	q = queue.Queue()
+	q.put(0)
+	parent = []
+	parent.append(0)
+	if(start[0] == end):
+		return True
+	while(q.empty() == False):
+		cnt = q.get()
+		p = st[cnt]
+		for x in range(0,len(p)):
+			L,U = p[x]
+			r1,c1 = cell(U)
+			r2,c2 = cell(L)
+			for d in range(4):
+				if isPossible(r1+dy[d],c1+dx[d]) and isPossible(r2+dy[d],c2+dx[d]):
+					L = cellInv(r2+dy[d],c2+dx[d])
+					U = cellInv(r1+dy[d],c1+dx[d])
+					state = p[0:x] + ((L,U),) + p[x+1:len(p)]
+					if state not in memo and isValid(p,(L,U),p[x]):
+						memo[state] = count
+						st.append(state)
+						parent.append(cnt)
+						q.put(count)
+						if(state[0] == end):
+							return path(parent,count)
+						count += 1
+	return []
+	
+end = (23,24)
+os.system('clear')
+p = solver(((0,1),(7,8),(16,17),(13,18),(15,20)),end)
+if p == []:
+	print('No solution')
+else:
+	for x in p:
+		q = st[x]
+		q = q + (end,)
+		mat = [col*['.'] for r in range(row)]
+		L,U = q[len(q)-1]
+		r1,c1 = cell(U)
+		r2,c2 = cell(L)
+		mat[r1][c1] = mat[r2][c2] = 'E'
+		L,U = q[0]
+		r1,c1 = cell(U)
+		r2,c2 = cell(L)
+		mat[r1][c1] = mat[r1][c2] = 'S'
+		for i in range(1,len(q)-1):
+			L,U = q[i]
+			r1,c1 = cell(U)
+			r2,c2 = cell(L)
+			mat[r1][c1] = mat[r2][c2] = '#'
+		visual.display(row,col,mat)
